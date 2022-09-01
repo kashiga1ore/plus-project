7bbcb74856f966da72011a7f8d6a29240e20752e
kashiga1ore <kashishdurgani13@gmail.com>
2022-09-01 19:52:17 +0800

    edited search engine

--- script.js
+++ script.js
@@ -41,7 +41,18 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=singapore&appid=6be29bbcb6ea40c2745402eba1192009&units=metric";
+function search(city) {
+  let apiKey = "6be29bbcb6ea40c2745402eba1192009";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("singapore");
